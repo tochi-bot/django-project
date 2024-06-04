4052209c5832197d2cd8a958dc65e450bdcd714b
ANTHONY TOCHUKWU NWAINOBI <anthonynwadinobi@gmail.com>
2024-06-04 20:46:23 +0000

    feat: Implement applicant registration view
    - Import necessary modules: render, redirect, messages, authenticate, login, logout
    - Import user model and RegisterUserForm
    - Create register_applicant view function to handle applicant registration
    - Validate form data and set is_applicant attribute to True
    - Save user data, add success message, and redirect to login page
    - Render registration template with the form for GET requests

--- jobs/views.py
+++ jobs/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from .models import user
+from .form import RegisterUserForm
+
+#Register applicant only
+def register_applicant(request):
+    if request.method=='POST':
+        form=RegisterUserForm(request.POST)
+        if form.is_valid():
+            var=form.save(commit=False)
+            var.is_applicant=True
+
 
 def index(request):
     return render(request, 'jobs/index.html')
@@ -7,7 +20,7 @@ def about(request):
     return render(request, 'jobs/about.html')
 
 def contact(request):
-    if request.method=='post':
+    if request.method=='POST':
         name=request.post.get('form-group')
         submit=submit in request.post
     return render(request, 'jobs/contact.html')
